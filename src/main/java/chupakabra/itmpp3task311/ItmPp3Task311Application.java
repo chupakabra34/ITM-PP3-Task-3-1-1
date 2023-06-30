cf73f0d45166d4a98937180bd840f7cc36f7c676
Sergei Prekrasnov <sap685@gmail.com>
2023-07-01 00:52:58 +0300

    Start

---
+++ src/main/java/chupakabra/itmpp3task311/ItmPp3Task311Application.java
@@ -0,0 +1,13 @@
+package chupakabra.itmpp3task311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItmPp3Task311Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItmPp3Task311Application.class, args);
+    }
+
+}
